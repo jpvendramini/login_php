b1f12d5bb0c931beff857f6253f25860a84d6037
JP Vendramini <joaopedrovendraminiv90@gmail.com>
2021-06-16 08:12:18 -0300

    style

--- index.php
+++ index.php
@@ -16,16 +16,64 @@
 </head>
 <body>
     <style>
-    
-    
-    
+        *{
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; 
+            color: white;    
+            box-sizing: border-box;       
+        }
+        body{
+            background-color: lightgrey;
+        }
+        #container{            
+            width: 900px;
+            height: 100px;
+            margin: auto;          
+            background-color: darkgrey;     
+            border-radius: 10px;       
+        }
+        #barraPrincipal > h1{
+            position: relative;
+            top: 25px;
+            padding-left: 30px;            
+        }
+        .elementosBarra{            
+            position: relative;
+            padding-right: 20px;
+            padding-top: 10px;
+            right: -750px;
+            top: -50px;
+        }
+        .artigo{      
+            position: relative;   
+            top: -10px;   
+            width: 300px;
+            height: 300px;
+            background-color: darkgrey;
+            float: left;            
+            border-radius: 20px;
+        }
+        .containerArtigos{
+            position: absolute;
+        }
     </style>
     <div id="container">
-        <a href="logout.php">Logout</a>
-        <h1>This is the index page</h1>
+        <div id="barraPrincipal">
+            <h1>My PHP page ;)</h1>
+            <div class="elementosBarra">
+                <a href="logout.php">Logout</a>
+                <br>
+                <p>Hello, <?php echo $user_data['user_name'];?>!</p>
+            </div>
 
-        <br>
-        Hello, <?php echo $user_data['user_name'];?>!
+        </div>
+        <div class="containerArtigos">
+            <div class="artigo"></div>
+            <div class="artigo"></div>
+            <div class="artigo"></div>
+        </div>
     </div>
+    <footer>
+
+    </footer>
 </body>
 </html>
